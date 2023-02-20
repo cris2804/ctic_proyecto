e4c608c3961eba988fde75e81167ef4e0d27889f
Abraham ✨ <aberrospic@uni.pe>
2023-02-20 13:55:54 -0500

    más cambios

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,3 @@
-import logo from './images/logosc.png'
 import logov2 from './images/logo_version_grafana.png'
 import './css/Dashboard.css'
 
@@ -11,7 +10,7 @@ export default function Dashboard(){
                 <iframe src="http://181.176.48.200:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&panelId=4" width="100%" height="17%" frameborder="0"></iframe>
                 {/*<iframe src="http://181.176.48.200:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&from=1675695935699&to=1675782335699&panelId=10" width="450" height="200" frameborder="0"></iframe>*/}
                 <div className='container-dashboard'>
-                    <div className='container-dashboard-title'>DASHBOARD</div>
+                    <div className='container-dashboard-title'>Dashboard</div>
                     <a href='/universidad-nacional-de-ingenieria'>Universidad Nacional de Ingeniería</a>
                 </div>
                 <iframe src="http://181.176.48.200:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&panelId=12" width="100%" height="20%" frameborder="0"></iframe>
--- src/pages/University.js
+++ src/pages/University.js
@@ -1,18 +1,13 @@
-import logo from './images/logosc.png'
+import logov2 from './images/logo_version_grafana.png'
 import './css/University.css'
-import logouni from './images/logouni.png'
+import logouniv2 from './images/logouni_v2.png'
 
 export default function University(){
-    return(<>
-        <div className="container-nav">
-            <div className='container-image'>
-                <a href='/'><img src={logo} alt='logo'/> </a>
-            </div>
-            <div className='container-ruta-university'>
-                <a href='/universidad-nacional-de-ingenieria'>UNIVERSIDAD NACIONAL DE INGENIERIA</a>
-            </div>
-                {/*<iframe src="http://181.176.48.200:3000/d-solo/2-sxtM24k/carga-viral-ctic?orgId=1&from=1675784962255&to=1675871362255&panelId=6" width="100%" height="200" frameborder="0"></iframe>*/}
+    return(<div>
+        <div className='container-image'>
+            <a href='/'><img src={logov2} alt='logov2' height={100}/> </a>
         </div>
+            {/*<iframe src="http://181.176.48.200:3000/d-solo/2-sxtM24k/carga-viral-ctic?orgId=1&from=1675784962255&to=1675871362255&panelId=6" width="100%" height="200" frameborder="0"></iframe>*/}
         <div className="container-university-main">
             <div className='container-logo-map'>
                 {/*<a href='/'><img src={logo} alt="logo"/></a>*/}
@@ -20,19 +15,24 @@ export default function University(){
             </div>
             <div className='container-name-university-dash'>
                 <div className='container-name-logo-university'>
-                    <img src={logouni} alt='imagen-logo'/>
-                    <div className='container-name-university'>
-                        <div>UNIVERSIDAD</div>
-                        <div>NACIONAL DE</div>
-                        <div>INGENIERIA</div>
-                    </div>
+                    <img src={logouniv2} alt='imagen-logo-v2'/>
                 </div>
                 <div className='container-dash'>
-                    <div className='container-title-dash-2'>CARGA VIRAL</div>
+                    <div className='container-title-dash-2'>Carga Viral</div>
                     <a href='/carga-viral-CTIC'>Carga Viral (CTIC)</a>
                     <a href='/carga-viral-comedor-universitario'>Carga Viral (Comedor Universitario)</a>
                 </div>
+                <div className='container-dash'>
+                    <div className='container-title-dash-2'>Calidad de Aire</div>
+                    <a href='/carga-viral-CTIC'>Calidad de Aire (CTIC)</a>
+                    <a href='/carga-viral-comedor-universitario'>Calidad de Aire (Puerta 3)</a>
+                    <a href='/carga-viral-CTIC'>Calidad de Aire (Puerta 5)</a>
+                </div>
+                <div className='container-dash'>
+                    <div className='container-title-dash-2'>Smart Parking</div>
+                    <a href='/carga-viral-CTIC'>Smart Parking (CTIC)</a>
+                </div>
             </div>
         </div>
-        </>);
+        </div>);
 }
