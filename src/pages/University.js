331a3cc6f9660916e76ccfc4f8c559fb9e930008
FranklinRiveraGranados <friverag@uni.pe>
2023-02-23 09:46:28 -0500

    Agregando tamaño del mapa en vista uni

--- src/pages/University.js
+++ src/pages/University.js
@@ -11,7 +11,7 @@ export default function University(){
         <div className="container-university-main">
             <div className='container-logo-map'>
                 {/*<a href='/'><img src={logo} alt="logo"/></a>*/}
-                <iframe src="http://181.176.48.200:3000/d-solo/7bhq8tc4z/universidad-nacional-de-ingenieria?orgId=1&from=1675781192877&to=1675867592877&panelId=14" width="100%" height="800" frameborder="0"></iframe>
+                <iframe src="http://181.176.48.200:3000/d-solo/7bhq8tc4z/universidad-nacional-de-ingenieria?orgId=1&from=1675781192877&to=1675867592877&panelId=14" width="100%" height="100%" frameborder="0"></iframe>
             </div>
             <div className='container-name-university-dash'>
                 <div className='container-name-logo-university'>
