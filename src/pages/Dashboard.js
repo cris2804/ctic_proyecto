6e3f74e6665c1846e26d883be16317945d5f4859
FranklinRiveraGranados <friverag@uni.pe>
2023-02-24 09:34:38 -0500

    agregando ancho 100% en clase cuenta personas

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,7 +8,8 @@ export default function Dashboard(){
         <div className='container-main'>
             <div className='container-logo-time-aforo-dashboard'>
                 <img src={logov2} alt="logov2"/>
-                <iframe src="http://181.176.48.200:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&panelId=4" width="100%" height="17%" frameborder="0"></iframe>
+                {/*<iframe src="http://181.176.48.200:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&panelId=4" width="100%" height="17%" frameborder="0"></iframe>*/}
+                <iframe src="http://192.168.52.232:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&panelId=4" width="100%" height="17%" frameborder="0"></iframe>
                 {/*<iframe src="http://181.176.48.200:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&from=1675695935699&to=1675782335699&panelId=10" width="450" height="200" frameborder="0"></iframe>*/}
                 <div className='container-dashboard'>
                     <div className='container-dashboard-title'>Dashboard</div>
@@ -38,7 +39,8 @@ export default function Dashboard(){
                 </div>
             </div>
             <div className='container-map-dashboard'>
-                <iframe src="http://181.176.48.200:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&from=1675696115267&to=1675782515267&panelId=2" width="100%" height="100%" frameborder="0"></iframe>
+                {/*<iframe src="http://181.176.48.200:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&from=1675696115267&to=1675782515267&panelId=2" width="100%" height="100%" frameborder="0"></iframe>*/}
+                <iframe src="http://192.168.52.232:3000/d-solo/XS78ahc4k/home?orgId=1&refresh=1s&panelId=2" width="100%" height="100%" frameborder="0"></iframe>
             </div>
         </div>
     </div>);
