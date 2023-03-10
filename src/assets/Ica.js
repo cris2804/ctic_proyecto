2a7e949d47b86aac4170b29e584d0f30834bb157
FranklinRiveraGranados <friverag@uni.pe>
2023-03-10 11:47:19 -0500

    Agregando vista de los rango de ica al posisionar el mouse en el valor del ica

---
+++ src/assets/Ica.js
@@ -0,0 +1,32 @@
+export const Ica = [
+    {
+        "nombre": "Bueno",
+        "color": "#9AD64D",
+        "rango": "0-50",
+    },
+    {
+        "nombre": "Moderado",
+        "color": "#F8CD38",
+        "rango": "50-100",
+    },
+    {
+        "nombre": "Perjudicial para grupos sensibles",
+        "color": "#F88F48",
+        "rango": "100-150",
+    },
+    {
+        "nombre": "Perjudicial",
+        "color": "#F55D5E",
+        "rango": "150-200",
+    },
+    {
+        "nombre": "Muy Perjudciail",
+        "color": "#A070B6",
+        "rango": "200-300",
+    },
+    {
+        "nombre": "Peligroso",
+        "color": "#A06A7B",
+        "rango": "300-500",
+    }
+]
--- src/pages/DetalleCA.js
+++ src/pages/DetalleCA.js
@@ -1,8 +1,19 @@
 import './css/DetalleCA.css';
 import happy from '../components/images/happy.png';
+import { useState } from 'react';
+import { Ica } from '../assets/Ica';
 
 export default function DetalleCA(){
+    const [isHovered, setIsHovered] = useState(false);
 
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+        
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+    
     return (
         <div className='container__detalle__ca'>
             <div>
@@ -39,19 +50,28 @@ export default function DetalleCA(){
                 <div className='container__detalle__ca__right'>
                     <div className='container__estado__ca'>
                         <div className='container__valor__tipo'>
-                            <div className='container__valor'>104</div>
+                            <div className='container__valor' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>104</div>
                             <div className='container__indice__tipo'>
                                 <div className='indice'>ÍNDICE ICA EN VIVO</div>
-                                <div className='tipo'>Perjudicial Para Grupos Sensibles</div>
+                                <div className='tipo'>Bueno</div>
                             </div>
                         </div>
                         <div className='logo__imagen'>
                             <img src={happy} alt='logo-happy'/>
                         </div>
+                        <div className={isHovered ? 'container__detalles__ica':'container__detalles__ica2'}>
+                            {Ica.map((element, i) => {
+                                return(<div className='container__ica__rango__nombre'>
+                                    <div style={{background: element.color}}>{element.rango}</div>
+                                    <div key={i}>{element.nombre}</div>
+                                
+                                </div>
+                                )
+                            })}
+                        </div>
                     </div>
 
                     <div>
-                        vivir
 
                     </div>
                 </div>
