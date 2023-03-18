9cfbc9e688d026d5c7e3eabd74a49bcefc9ab5b2
FranklinRiveraGranados <friverag@uni.pe>
2023-03-17 19:49:10 -0500

    Agregando cambios

--- src/pages/CalidadAirePuerta3.js
+++ src/pages/CalidadAirePuerta3.js
@@ -1,12 +1,32 @@
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
+import './css/CalidadAirePuerta3.css';
 
 export default function CalidadAirePuerta3(){
-    const [date, setDate] = useState(new Date());
+  const [date1, setDate1] = useState(new Date());
+  const [date2, setDate2] = useState(new Date());
+  const [showCalendar1, setShowCalendar1] = useState(false);
+  const [showCalendar2, setShowCalendar2] = useState(false);
 
-  const onChange = (date) => {
-    setDate(date);
+  const onChange1 = (date) => {
+    setDate1(date);
+    setShowCalendar1(!showCalendar1);
+  }
+  const onChange2 = (date) => {
+    setDate2(date);
+    setShowCalendar2(!showCalendar2);
+  }
+
+  const toggleCalendar1 = () => {
+    setShowCalendar1(!showCalendar1);
+  }
+  const toggleCalendar2 = () => {
+    setShowCalendar2(!showCalendar2);
+  }
+
+  const handleMostrar = () => {
+    console.log("De: "+date1.toLocaleDateString(),"Hasta: "+date2.toLocaleDateString());
   }
 
     return(
