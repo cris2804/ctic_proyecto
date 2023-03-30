7bfc51c396fcd976869abe7e805867d9b51b4250
FranklinRiveraGranados <friverag@uni.pe>
2023-03-30 12:28:59 -0500

    Agregando cambios

--- src/pages/DetalleCAI.js
+++ src/pages/DetalleCAI.js
@@ -4,10 +4,10 @@ export default function DetalleCAI(){
     return(
         <div className="container__main__cai">
             <div className="container__main__cai__left">
-                left
+                
             </div>
             <div className="container__main__cai__right">
-                right
+                
             </div>
         </div>
     )
