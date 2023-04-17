ca72f51cd815910b728ea3f5c7600464dab51bd6
FranklinRiveraGranados <friverag@uni.pe>
2023-04-17 09:34:31 -0500

    Centrando los valores en la legenda

--- src/App.js
+++ src/App.js
@@ -1,129 +1,197 @@
-import './App.css';
-import {useState, useEffect} from 'react';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import University from './pages/University';
-import CargaViralCTIC from './pages/CargaViralCTIC';
-import CargaViralComedor from './pages/CargaViralComedor';
-import CalidadAireCTIC from './pages/CalidadAireCTIC';
-import CalidadAirePuerta5 from './pages/CalidadAirePuerta5';
-import CalidadAirePuerta3 from './pages/CalidadAirePuerta3';
-import SmartParking from './pages/SmartParking';
-import Main from './pages/Main'
-import DetalleCA from './pages/DetalleCA';
-import FaqCAI from './pages/FaqCAI';	
-import FaqCAE from './pages/FaqCAE';
-import Grafico3 from './components/Grafico3';
-import DetalleCAI from './pages/DetalleCAI';
-import CSV from './pages/CSV';
-import Inicio from './images/grid.svg';
-import carga from './images/carga.png';
-import cae from './images/cae3.png';
-import logo from './images/logo.png';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import University from "./pages/University";
+import CargaViralCTIC from "./pages/CargaViralCTIC";
+import CargaViralComedor from "./pages/CargaViralComedor";
+import CalidadAireCTIC from "./pages/CalidadAireCTIC";
+import CalidadAirePuerta5 from "./pages/CalidadAirePuerta5";
+import CalidadAirePuerta3 from "./pages/CalidadAirePuerta3";
+import SmartParking from "./pages/SmartParking";
+import Main from "./pages/Main";
+import DetalleCA from "./pages/DetalleCA";
+import FaqCAI from "./pages/FaqCAI";
+import FaqCAE from "./pages/FaqCAE";
+import Grafico3 from "./components/Grafico3";
+import DetalleCAI from "./pages/DetalleCAI";
+import CSV from "./pages/CSV";
+import Inicio from "./images/grid.svg";
+import carga from "./images/carga.png";
+import cae from "./images/cae3.png";
+import logo from "./images/logo.png";
 
 function App() {
   const [isExpanded, setExpendState] = useState(false);
-	const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState(null);
   const [actual, setActual] = useState(0);
 
-
   const toggle = (i) => {
-    if(selected ===  i){
-    	setSelected(null)
-	}else if((i === 1 || i === 2) && !isExpanded){
-      setSelected(i)
-	  setExpendState(true)
-    }else{
-		setSelected(i)
-	}
-    setActual(i)
-  }
+    if (selected === i) {
+      setSelected(null);
+    } else if ((i === 1 || i === 2) && !isExpanded) {
+      setSelected(i);
+      setExpendState(true);
+    } else {
+      setSelected(i);
+    }
+    setActual(i);
+  };
   useEffect(() => {
-		if(!isExpanded) setSelected(null)
-	},[isExpanded]);
+    if (!isExpanded) setSelected(null);
+  }, [isExpanded]);
 
   return (
     <div className="main">
-      <div className={isExpanded ? "container__sidebar__":"container__sidebar__nx"}>
-			<div className={isExpanded ? "side-nav-container" : "side-nav-container side-nav-container-NX"}>
-				<div className="nav-upper">
-					<div className="nav-heading">
-						{isExpanded && (
-							<div className="nav-brand">
-								<img src={logo} alt="" />
-							</div>
-						)}
-						<button
-							className={isExpanded ? "hamburger hamburger-in" : "hamburger hamburger-out"}
-							onClick={() => setExpendState(!isExpanded)}>
-							<span></span>
-							<span></span>
-							<span></span>
-						</button>
-					</div>
-					<div className="nav-menu">
-						
-						<a className={isExpanded ? "menu-item" : "menu-item menu-item-NX"} style={{background: `${actual === 0 ? "none":"none"}`}} onClick={() => toggle(0)} href="/">
-							<img className="menu-item-icon" src={Inicio} alt=""/>
-							{isExpanded && <p>Inicio</p>}
-						</a>
-						
-						<div className={isExpanded ? "menu-item" : "menu-item menu-item-NX"} style={{background: `${actual === 1 ? "none":"none"}`}} onClick={() => toggle(1)}>
-							<img className="menu-item-icon" src={cae} alt=""/>
-							{isExpanded && <p>Calidad del aire en interiores</p>}
-						</div>
-						<div className={selected === 1 ? 'content show2' : 'content'}>
-							<a className={isExpanded ? "menu-item" : "menu-item menu-item-NX"} href="/calidad-del-aire-interiores-ctic?id=Oficina de Administración">
-								{isExpanded && <p>CTIC</p>}
-							</a>
-							<a className={isExpanded ? "menu-item" : "menu-item menu-item-NX"} href="/calidad-aire-interiores-comedor">
-								{isExpanded && <p>Comedor Universitario</p>}
-							</a>
-						</div>
+      <div
+        className={
+          isExpanded ? "container__sidebar__" : "container__sidebar__nx"
+        }
+      >
+        <div
+          className={
+            isExpanded
+              ? "side-nav-container"
+              : "side-nav-container side-nav-container-NX"
+          }
+        >
+          <div className="nav-upper">
+            <div className="nav-heading">
+              {isExpanded && (
+                <div className="nav-brand">
+                  <img src={logo} alt="" />
+                </div>
+              )}
+              <button
+                className={
+                  isExpanded
+                    ? "hamburger hamburger-in"
+                    : "hamburger hamburger-out"
+                }
+                onClick={() => setExpendState(!isExpanded)}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+            <div className="nav-menu">
+              <a
+                className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+                style={{ background: `${actual === 0 ? "none" : "none"}` }}
+                onClick={() => toggle(0)}
+                href="/"
+              >
+                <img className="menu-item-icon" src={Inicio} alt="" />
+                {isExpanded && <p>Inicio</p>}
+              </a>
 
-						<div className={isExpanded ? "menu-item" : "menu-item menu-item-NX"} style={{background: `${actual === 2 ? "none":"none"}`}} onClick={() => toggle(2)}>
-							<img className="menu-item-icon" src={carga} alt=""/>
-							{isExpanded && <p>Calidad del aire en exteriores</p>}
-						</div>
-						<div className={selected === 2 ? 'content show2' : 'content'}>
-							<a className={isExpanded ? "menu-item" : "menu-item menu-item-NX"} href="/calidad-del-aire-exteriores?id=ctic">
-								{isExpanded && <p>CTIC</p>}
-							</a>
-							<a className={isExpanded ? "menu-item" : "menu-item menu-item-NX"} href="/calidad-del-aire-exteriores?id=puerta 3">
-								{isExpanded && <p>Puerta 3</p>}
-							</a>
-							<a className={isExpanded ? "menu-item" : "menu-item menu-item-NX"} href="/calidad-del-aire-exteriores?id=puerta 5">
-								{isExpanded && <p>Puerta 5</p>}
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-      <div className={isExpanded ? "container__main__":"container__main__nx"}>
+              <div
+                className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+                style={{ background: `${actual === 1 ? "none" : "none"}` }}
+                onClick={() => toggle(1)}
+              >
+                <img className="menu-item-icon" src={cae} alt="" />
+                {isExpanded && <p>Calidad del aire en interiores</p>}
+              </div>
+              <div className={selected === 1 ? "content show2" : "content"}>
+                <a
+                  className={
+                    isExpanded ? "menu-item" : "menu-item menu-item-NX"
+                  }
+                  href="/calidad-del-aire-interiores-ctic?id=Oficina de Administración"
+                >
+                  {isExpanded && <p>CTIC</p>}
+                </a>
+                <a
+                  className={
+                    isExpanded ? "menu-item" : "menu-item menu-item-NX"
+                  }
+                  href="/calidad-aire-interiores-comedor"
+                >
+                  {isExpanded && <p>Comedor Universitario</p>}
+                </a>
+              </div>
+
+              <div
+                className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+                style={{ background: `${actual === 2 ? "none" : "none"}` }}
+                onClick={() => toggle(2)}
+              >
+                <img className="menu-item-icon" src={carga} alt="" />
+                {isExpanded && <p>Calidad del aire en exteriores</p>}
+              </div>
+              <div className={selected === 2 ? "content show2" : "content"}>
+                <a
+                  className={
+                    isExpanded ? "menu-item" : "menu-item menu-item-NX"
+                  }
+                  href="/calidad-del-aire-exteriores?id=ctic"
+                >
+                  {isExpanded && <p>CTIC</p>}
+                </a>
+                <a
+                  className={
+                    isExpanded ? "menu-item" : "menu-item menu-item-NX"
+                  }
+                  href="/calidad-del-aire-exteriores?id=puerta 3"
+                >
+                  {isExpanded && <p>Puerta 3</p>}
+                </a>
+                <a
+                  className={
+                    isExpanded ? "menu-item" : "menu-item menu-item-NX"
+                  }
+                  href="/calidad-del-aire-exteriores?id=puerta 5"
+                >
+                  {isExpanded && <p>Puerta 5</p>}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={isExpanded ? "container__main__" : "container__main__nx"}>
         <Router>
           <Routes>
+            <Route path="/" element={<Main />} />
+            <Route
+              path="/calidad-del-aire-exteriores"
+              element={<DetalleCA />}
+            />
+            <Route
+              path="/calidad-del-aire-interiores-ctic"
+              element={<DetalleCAI />}
+            />
+            <Route path="/faq-carga-viral" element={<FaqCAI />} />
+            <Route path="/faq-calidad-de-aire" element={<FaqCAE />} />
+            <Route path="/detalle-carga-viral" element={<DetalleCA />} />
+            <Route path="/grafico-semana" element={<Grafico3 />} />
+            <Route path="/csv" element={<CSV />} />
 
-          <Route path='/' element={<Main/>}/>
-          <Route path='/calidad-del-aire-exteriores' element={<DetalleCA/>}/>
-          <Route path='/calidad-del-aire-interiores-ctic' element={<DetalleCAI/>}/>
-          <Route path='/faq-carga-viral' element={<FaqCAI/>}/>	
-          <Route path='/faq-calidad-de-aire' element={<FaqCAE/>}/>
-          <Route path='/detalle-carga-viral' element={<DetalleCA/>}/>
-          <Route path='/grafico-semana' element={<Grafico3/>}/>
-		  <Route path='/csv' element={<CSV/>}/>
-
-            <Route path='/dashboard' element={<Dashboard/>}/>  
-            <Route path='/universidad-nacional-de-ingenieria' element={<University/>}/> 
-            <Route path='/carga-viral-CTIC' element={<CargaViralCTIC/>}/>
-            <Route path='/carga-viral-comedor-universitario' element={<CargaViralComedor/>}/>
-            <Route path='/calidad-de-aire-ctic' element={<CalidadAireCTIC/>}/>
-            <Route path='/calidad-de-aire-puerta-5' element={<CalidadAirePuerta5/>}/>
-            <Route path='/calidad-de-aire-puerta-3' element={<CalidadAirePuerta3/>}/>
-            <Route path='/smart-parking' element={<SmartParking/>}/>
-            <Route path='/localización/101' element={<SmartParking/>}/>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+              path="/universidad-nacional-de-ingenieria"
+              element={<University />}
+            />
+            <Route path="/carga-viral-CTIC" element={<CargaViralCTIC />} />
+            <Route
+              path="/carga-viral-comedor-universitario"
+              element={<CargaViralComedor />}
+            />
+            <Route path="/calidad-de-aire-ctic" element={<CalidadAireCTIC />} />
+            <Route
+              path="/calidad-de-aire-puerta-5"
+              element={<CalidadAirePuerta5 />}
+            />
+            <Route
+              path="/calidad-de-aire-puerta-3"
+              element={<CalidadAirePuerta3 />}
+            />
+            <Route path="/smart-parking" element={<SmartParking />} />
+            <Route path="/localización/101" element={<SmartParking />} />
           </Routes>
-        </Router>       
+        </Router>
       </div>
     </div>
   );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,34 +1,34 @@
-import './css/Main.css';
-import { ubicaciones } from '../assets/ubicaciones';
-import { ubicacionesCV } from '../assets/ubicacionesCV';
-import { useState } from 'react';
-import calidad from '../assets/calidad.png';
-import carga from '../assets/carga.png';
-import {MapContainer, TileLayer} from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import { Marker } from "react-leaflet"
+import "./css/Main.css";
+import { ubicaciones } from "../assets/ubicaciones";
+import { ubicacionesCV } from "../assets/ubicacionesCV";
+import { useState } from "react";
+import calidad from "../assets/calidad.png";
+import carga from "../assets/carga.png";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Marker } from "react-leaflet";
 import L from "leaflet";
-import logoclose from './images/logoclose.png';
+import logoclose from "./images/logoclose.png";
 
-import Detalles from '../components/Detalles'; 
-import AyudaCAE from '../components/AyudaCAE';
-import AyudaCAI from '../components/AyudaCAI'; 
-import DetallesI from '../components/DetallesI';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import { rangoscae } from '../assets/rangoscae';
-import { rangoscai } from '../assets/rangoscai';
+import Detalles from "../components/Detalles";
+import AyudaCAE from "../components/AyudaCAE";
+import AyudaCAI from "../components/AyudaCAI";
+import DetallesI from "../components/DetallesI";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import { rangoscae } from "../assets/rangoscae";
+import { rangoscai } from "../assets/rangoscai";
 
-const ubiCentro = ['-12.020381', '-77.049178']
+const ubiCentro = ["-12.020381", "-77.049178"];
 
-function changeOption(i, id, estado){
-  if(i === 0){
-    return <Detalles id={id}/>
-  }else if(i === 1){
-    if(id === "cv-ctic") return <DetallesI id={id}/>
-    else return "comedor universitario"
-  }else if(i === 2){
-    if(estado) return <AyudaCAE/>
-    else return <AyudaCAI/> 
+function changeOption(i, id, estado) {
+  if (i === 0) {
+    return <Detalles id={id} />;
+  } else if (i === 1) {
+    if (id === "cv-ctic") return <DetallesI id={id} />;
+    else return "comedor universitario";
+  } else if (i === 2) {
+    if (estado) return <AyudaCAE />;
+    else return <AyudaCAI />;
   }
 }
 
@@ -38,121 +38,160 @@ function Main() {
   const [estadoCA, setEstadoCA] = useState(true);
   const [estadoCV, setEstadoCV] = useState(false);
   /*--- */
-  const [id, setId] = useState('');
+  const [id, setId] = useState("");
   const [bol, setBol] = useState(false);
 
   /*--- */
   const [i, setI] = useState(0);
 
   /*--- */
-  const [rangos, setRangos] = useState(rangoscae)
+  const [rangos, setRangos] = useState(rangoscae);
 
   /*-- */
   const handleCambiarCA = (i) => {
-    setUbi(ubicaciones)
-    setRangos(rangoscae)
+    setUbi(ubicaciones);
+    setRangos(rangoscae);
 
-    if(estadoCA === false && estadoCV === true){
+    if (estadoCA === false && estadoCV === true) {
       setEstadoCA(true);
       setEstadoCV(false);
     }
     //se oculta el panel cuando pasamos de cae a cai
     if (i !== 2) setBol(false);
     setI(i);
-  }
+  };
   const handleCambiarCV = (i) => {
-    setUbi(ubicacionesCV)
-    setRangos(rangoscai)
-    
-    if(estadoCV === false && estadoCA === true){
+    setUbi(ubicacionesCV);
+    setRangos(rangoscai);
+
+    if (estadoCV === false && estadoCA === true) {
       setEstadoCV(true);
       setEstadoCA(false);
     }
     //se oculta el panel cuando pasamos de cai a cae
-    if(i!==2) setBol(false);
+    if (i !== 2) setBol(false);
     setI(i);
-  }
+  };
 
   /*--- */
-  const handleCerrar = ()=> {
-    if(i === 2){ //para cerrar el panel cuando estamos en ayuda
-      setI(5)
+  const handleCerrar = () => {
+    if (i === 2) {
+      //para cerrar el panel cuando estamos en ayuda
+      setI(5);
     }
     setBol(false);
-  }
-  const handleMostrar = (e,i) => {
-    setId(e)
-    setBol(true)
-    setI(i)
-  }
+  };
+  const handleMostrar = (e, i) => {
+    setId(e);
+    setBol(true);
+    setI(i);
+  };
 
   return (
-  <div className='container__main'>
-    <div className='cont container__map'>
-      <MapContainer center={ubiCentro} zoom={16}>
+    <div className="container__main">
+      <div className="cont container__map">
+        <MapContainer center={ubiCentro} zoom={16}>
           <TileLayer
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          {ubi.map((ubicacion) =>{
-            return <Marker 
-              key={ubicacion.clave}    
-              position={ubicacion.coordenadas} 
-              icon={L.icon({
-              iconUrl: ubicacion.logo,
-              iconRetinaUrl: ubicacion.logo,
-              iconSize: [40, 40],
-              className: "leaflet-venue-icon",
-              })}
-              eventHandlers={{ click: ()=>handleMostrar(ubicacion.clave, ubicacion.i) }} />
-            })}
-      </MapContainer>
+          {ubi.map((ubicacion) => {
+            return (
+              <Marker
+                key={ubicacion.clave}
+                position={ubicacion.coordenadas}
+                icon={L.icon({
+                  iconUrl: ubicacion.logo,
+                  iconRetinaUrl: ubicacion.logo,
+                  iconSize: [40, 40],
+                  className: "leaflet-venue-icon",
+                })}
+                eventHandlers={{
+                  click: () => handleMostrar(ubicacion.clave, ubicacion.i),
+                }}
+              />
+            );
+          })}
+        </MapContainer>
 
-      <div className='calidad__del__aire__carga__viral'>
-          <div className={estadoCA ? 'ca-cv-2':'ca-cv'} onClick={()=>handleCambiarCA(0)}>
+        <div className="calidad__del__aire__carga__viral">
+          <div
+            className={estadoCA ? "ca-cv-2" : "ca-cv"}
+            onClick={() => handleCambiarCA(0)}
+          >
             <img src={calidad} alt="logo calidad del aire" />
             <span>Calidad del Aire en Exteriores</span>
           </div>
-          <div className={estadoCV ? 'ca-cv-2':'ca-cv'} onClick={()=>handleCambiarCV(1)}>
-            <img src={carga} alt="carga viral"/>
+          <div
+            className={estadoCV ? "ca-cv-2" : "ca-cv"}
+            onClick={() => handleCambiarCV(1)}
+          >
+            <img src={carga} alt="carga viral" />
             <span>Calidad del Aire en Interiores</span>
           </div>
-      </div>
+        </div>
 
-      <div className='container__ica'>
-        <div className='container__ica_body'>
-          <div className='container__ica_legend_section'>
-            <div className='map-legend-title'>
-              <span class="type-subtitle-3 text-smoke-120"> {estadoCA ? "INCA" : "ppm"} </span>
-              {/*<span class="type-subtitle-3 text-smoke-60"> Mediciones más recientes</span>*/}
+        <div className="container__ica">
+          <div className="container__ica_body">
+            <div className="container__ica_legend_section">
+              <div className="map-legend-title">
+                <span class="type-subtitle-3 text-smoke-120">
+                  {" "}
+                  {estadoCA ? "INCA" : "ppm"}{" "}
+                </span>
+                {/*<span class="type-subtitle-3 text-smoke-60"> Mediciones más recientes</span>*/}
+              </div>
+              <div className="map-legend-bar">
+                {rangos.map((rang, i) => {
+                  return (
+                    <div
+                      style={{
+                        flex: "1 1 0%",
+                        borderRight: `solid 1px #dedede`,
+                        backgroundImage: `linear-gradient(to right, ${rang.color1}, ${rang.color2})`,
+                      }}
+                    ></div>
+                  );
+                })}
+              </div>
+              <div className="map-legend-bar-labels">
+                {rangos.map((rang, i) => {
+                  return (
+                    <div class="type-body-4" style={{ flex: "1 1 0%", position: "relative", height: "20px"}}>
+                      <span style={{position: "absolute", height: "100%", transform: "translateX(-50%)"}}>{rang.valor}</span>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-            <div className='map-legend-bar'>
-              {rangos.map((rang,i)=>{
-                return <div style={{ flex: "1 1 0%", borderRight: `solid 1px #dedede`, backgroundImage: `linear-gradient(to right, ${rang.color1}, ${rang.color2})`}}></div>
-              })}
+            <div
+              className="container__ica_help_section"
+              onClick={() => setI(2)}
+            >
+              <button class="button-reset">
+                <span class="legend-help">
+                  <HelpOutlineIcon />
+                </span>
+                <span> Ayuda </span>
+              </button>
             </div>
-            <div className='map-legend-bar-labels'>
-            {rangos.map((rang,i)=>{
-                return <span class="type-body-4" style={{flex: "1 1 0%"}}>{rang.valor}</span>
-              })}
-            </div>
-          </div>
-          <div className='container__ica_help_section' onClick={()=>setI(2)}>
-            <button class="button-reset">
-              <span class="legend-help"><HelpOutlineIcon/></span>
-              <span> Ayuda </span>
-            </button>
           </div>
         </div>
-      </div>
 
-      <div className={bol || i === 2 ? 'container__datos__ca__cv':'container__datos__ca__cv2'}>
-        <div className='container__logo__close'><img src={logoclose} alt="logo-close" onClick={handleCerrar}/></div>
-        {changeOption(i, id,estadoCA)}
+        <div
+          className={
+            bol || i === 2
+              ? "container__datos__ca__cv"
+              : "container__datos__ca__cv2"
+          }
+        >
+          <div className="container__logo__close">
+            <img src={logoclose} alt="logo-close" onClick={handleCerrar} />
+          </div>
+          {changeOption(i, id, estadoCA)}
+        </div>
       </div>
     </div>
-    
-  </div>
   );
 }
 
