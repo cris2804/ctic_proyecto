02ed63895b8268e5674dbdf4a3137a8455eaca93
FranklinRiveraGranados <friverag@uni.pe>
2023-05-26 09:29:08 -0500

    Agregando colores a graficos cai ctic

--- src/components/Grafico.js
+++ src/components/Grafico.js
@@ -20,7 +20,7 @@ const Grafico = ({ nombre, datos, id }) => {
   }, [datos]);*/
 
   const CustomDot = ({ cx, cy, stroke, fill, payload, value }) => {
-    if (value < 50 && value > 0) {
+    if (value <= 800 && value > 0) {
       return (
         <svg
           x={cx - 5}
@@ -33,7 +33,7 @@ const Grafico = ({ nombre, datos, id }) => {
           <circle cx={5} cy={5} r={5} fill="#9AD64D" />
         </svg>
       );
-    } else if (value < 100 && value > 50) {
+    } else if (value <= 1000 && value > 800) {
       return (
         <svg
           x={cx - 5}
@@ -46,7 +46,7 @@ const Grafico = ({ nombre, datos, id }) => {
           <circle cx={5} cy={5} r={5} fill="#F8CD38" />
         </svg>
       );
-    } else if (value < 150 && value > 100) {
+    } else if (value > 1000) {
       return (
         <svg
           x={cx - 5}
@@ -57,45 +57,6 @@ const Grafico = ({ nombre, datos, id }) => {
           viewBox="0 0 10 10"
         >
           <circle cx={5} cy={5} r={5} fill="#F88F48" />
-        </svg>
-      );
-    } else if (value < 200 && value > 150) {
-      return (
-        <svg
-          x={cx - 5}
-          y={cy - 5}
-          width={10}
-          height={10}
-          fill="#F55D5E"
-          viewBox="0 0 10 10"
-        >
-          <circle cx={5} cy={5} r={5} fill="#F55D5E" />
-        </svg>
-      );
-    } else if (value < 300 && value > 200) {
-      return (
-        <svg
-          x={cx - 5}
-          y={cy - 5}
-          width={10}
-          height={10}
-          fill="#A070B6"
-          viewBox="0 0 10 10"
-        >
-          <circle cx={5} cy={5} r={5} fill="#A070B6" />
-        </svg>
-      );
-    } else if (value < 500 && value > 300) {
-      return (
-        <svg
-          x={cx - 5}
-          y={cy - 5}
-          width={10}
-          height={10}
-          fill="#A06A7B"
-          viewBox="0 0 10 10"
-        >
-          <circle cx={5} cy={5} r={5} fill="#A06A7B" />
         </svg>
       );
     }
@@ -118,7 +79,7 @@ const Grafico = ({ nombre, datos, id }) => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `http://192.168.52.232:9090/api/v1/carga-viral/${id}?last=5`
+          `http://192.168.52.232:9090/api/v1/carga-viral/${id}?last=20`
         );
         const data = await response.json();
         const newData = data.map((item) => {
@@ -163,7 +124,7 @@ const Grafico = ({ nombre, datos, id }) => {
           <XAxis dataKey="time_index" reversed={false}>
             <Label value="Fecha" offset={0} position="bottom" fill="#000" />
           </XAxis>
-          <YAxis domain={[0, 600]}>
+          <YAxis domain={[0, 1200]}>
             <Label
               value={nombre}
               offset={5}
