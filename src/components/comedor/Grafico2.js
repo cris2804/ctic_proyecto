92a2c447baa956ef4c696b1af624b29f23f008cb
FranklinRiveraGranados <friverag@uni.pe>
2023-05-26 17:56:04 -0500

    cambios

--- src/components/comedor/Grafico2.js
+++ src/components/comedor/Grafico2.js
@@ -5,6 +5,15 @@ const data = [
   { time: '10:00:00', value: 200 },
   { time: '10:04:00', value: 900 },
   { time: '10:08:00', value: 1100 },
+  { time: '10:12:00', value: 600 },
+  { time: '10:16:00', value: 800 },
+  { time: '10:20:00', value: 820 },
+  { time: '10:24:00', value: 860 },
+  { time: '10:28:00', value: 920 },
+  { time: '10:32:00', value: 1000 },
+  { time: '10:36:00', value: 1050 },
+  { time: '10:40:00', value: 1084 },
+  { time: '10:44:00', value: 1200 },
   // ...
 ];
 
