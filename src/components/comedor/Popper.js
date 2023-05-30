6c3bd6cc3c6ca3dc3b1a4b1c013d1aa6583becee
FranklinRiveraGranados <friverag@uni.pe>
2023-05-30 18:27:27 -0500

    Agregando estilos

--- src/components/comedor/Popper.js
+++ src/components/comedor/Popper.js
@@ -3,7 +3,7 @@ import Card from '../Card'
 import {formatTimestamp} from './PeticionesComedor';
 export default function Popper({value,type,time,idb}) {
   return (
-    <div  style={{background: "#9AD64D",padding:'10px',borderRadius:'10px'}}>
+    <div  style={{background: "#fff",padding:'10px',border: 'solid 2px #000'}}>
         <div style={{textAlign:'center'}}><strong>Sensor {idb}</strong></div>
         <div >{type}: {value}</div>
         <div>Time: {formatTimestamp(time)}</div>
