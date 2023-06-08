2a7b5396202a1654ab57bdd77dc3752ed5d5ea2d
FranklinRiveraGranados <friverag@uni.pe>
2023-06-08 10:55:53 -0500

    Arreglando el poper

--- src/components/comedor/Popper.js
+++ src/components/comedor/Popper.js
@@ -6,8 +6,8 @@ export default function Popper({value,type,time,idb}) {
   return (
     <div  style={{background: "#fff",padding:'10px',border: 'solid 2px #000'}}>
         <div style={{textAlign:'center'}}><strong>Sensor {idb}</strong></div>
-        <div >{type}: {value}</div>
-        <div>Time: {formatTimestamp(time)}</div>
+        <div >CO₂: {value}</div> {/*<div >{type}: {value}</div> */}
+        <div>Tiempo: {formatTimestamp(time)}</div>
         {/*<div>Zona: Buena</div>*/}
     </div>
   )
