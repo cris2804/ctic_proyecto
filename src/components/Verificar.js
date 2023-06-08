4deca29fe7e0df1ac147ac942f28762663b708ce
FranklinRiveraGranados <friverag@uni.pe>
2023-06-08 12:43:37 -0500

    Agregando opción para no descargar

---
+++ src/components/Verificar.js
@@ -0,0 +1,33 @@
+function restarFechas(fecha1, fecha2, co2, temp, hum) {
+    // Convertir las fechas al formato "mm/d/yyyy" para que JavaScript las interprete correctamente
+    fecha1 = fecha1.split("/").reverse().join("-");
+    fecha2 = fecha2.split("/").reverse().join("-");
+  
+    // Calcular la diferencia en milisegundos
+    var diferencia = new Date(fecha2) - new Date(fecha1);
+  
+    // Convertir la diferencia de milisegundos a días
+    var diferenciaEnDias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  
+    var resultado = "";
+
+    if(diferenciaEnDias === 0){
+        if(!co2 && !temp && !hum) resultado = "Elija un rango de fechas válido y  los datos que desea descargar";
+        else resultado = "Elija un rango de fechas"; 
+    }else if(diferenciaEnDias < 0){
+        resultado = "Elija un rango de fechas válido";
+    }else {
+        if(!co2 && !temp && !hum) resultado = "Elija los datos que desea descargar";
+    }
+
+    return resultado;
+  }
+
+export default function Verificar({date1, date2, isChecked, isCheckedt, isCheckedh}){    
+
+    return(
+        <div style={{color: 'red', textAlign: 'center'}}>
+            {restarFechas(date1, date2, isChecked, isCheckedt, isCheckedh)}
+        </div>
+    )
+}
