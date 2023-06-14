4d9ced76115d62d8cdafae6c6a12711b34acbd38
FranklinRiveraGranados <friverag@uni.pe>
2023-06-14 17:11:13 -0500

    Agregando datos

--- src/pages/Cuentapersonas.js
+++ src/pages/Cuentapersonas.js
@@ -1,35 +1,33 @@
 import "./css/Cuentapersonas.css";
 import PopupZoom from "../components/PopupZoom";
 import Popup from "../components/Popup";
-import { useEffect,useState,useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 //import perfil from '../components/images/perfil.png';
 import io from "socket.io-client";
-import {Getip} from '../server/Getip';
-
+import { Getip } from "../server/Getip";
 
 const obtenerTurno = (horaLocal) => {
   //const fechaLocal = new Date();
   //const horaLocal = fechaLocal.getHours();
 
-
   if (horaLocal >= 7 && horaLocal <= 11) {
-    return 'Turno desayuno';
+    return "Turno desayuno";
   } else if (horaLocal >= 12 && horaLocal <= 14) {
-    return 'Turno almuerzo';
+    return "Turno almuerzo";
   } else if (horaLocal >= 16 && horaLocal <= 18) {
-    return 'Turno cena';
+    return "Turno cena";
   } else {
-    return 'Fuera de turno';
+    return "Fuera de turno";
   }
 };
 
 export default function Cuentapersonas() {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
   const [datactual, setDatactual] = useState([]);
   const [count, setCount] = useState(0);
   const [cargando, setCargando] = useState(false);
   const newDataRef = useRef(null);
-  const [getData, setGetData] = useState({nombres: "", img: ""});
+  const [getData, setGetData] = useState({ nombres: "", img: "" });
 
   let host = window.location.host;
 
@@ -42,35 +40,34 @@ export default function Cuentapersonas() {
       if (!cargando) {
         newDataRef.current = data.nombres.value;
         console.log(newDataRef.current);
-        //const enviarDatos = () => {
-          const url = "http://localhost:5000/image";
-          console.log("antes de fetch")
 
-          fetch(url, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ nombres: newDataRef.current}),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log("estoy en data")
-              ///////////
-              setGetData({
-                nombres: data.nombres,
-                msg: data.msg,
-                status: data.status,
-                //img: require('../img/'+newDataRef.current+'.jpg'),//'Base64_jpg(data.img)',
-              });
-              /////////////
-              console.log(data);
-            })
-            .catch((error) => {
-              console.error(error);
+        const url = "http://localhost:5000/image";
+        console.log("antes de fetch");
+
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ nombres: newDataRef.current }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("estoy en data");
+            ///////////
+            setGetData({
+              nombres: data.nombres,
+              msg: data.msg,
+              status: data.status,
             });
-        //};
-            console.log("sali de fetch")
+            /////////////
+            console.log(data);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+
+        console.log("sali de fetch");
         setShow(true);
         setCargando(true);
 
@@ -78,19 +75,17 @@ export default function Cuentapersonas() {
           setShow(false);
           setCargando(false);
           setCount((prevCount) => prevCount + 1);
-          /*setDatactual((prevData) => [
-            { nombres: newDataRef.current },
-            ...prevData,
-          ]);*/
-          setDatactual((prevData) => [
-            {
-              nombres: newDataRef.current,
-              //img: require('../img/'+newDataRef.current+'.jpg')
-              img: require('../components/images/perfil.png'),
-            },
-            ...prevData,
-          ]);
-          
+
+          if (getData.status === 0) {
+            setDatactual((prevData) => [
+              {
+                nombres: newDataRef.current,
+                //img: require('../img/'+newDataRef.current+'.jpg')
+                img: require("../components/images/perfil.png"),
+              },
+              ...prevData,
+            ]);
+          }
         }, 2000);
 
         return () => {
@@ -107,9 +102,7 @@ export default function Cuentapersonas() {
   return (
     <div className="container__main__cuenta__personas">
       <div className="container__left__cuenta__personas">
-        <div className="container__div__1">
-          {obtenerTurno()}
-        </div>
+        <div className="container__div__1">{obtenerTurno()}</div>
         <div className="container__div div1">
           <div>AFORO</div>
           <div className="container__valor">150</div>
@@ -124,14 +117,16 @@ export default function Cuentapersonas() {
           return <Popup nombres={d.nombres} perfil={d.img} key={i} />;
         })}
 
-        <div
-          className={show ? `container__popupzoom` : "container__popupzoom__"}
-        >
-          {/*<PopupZoom perfil={perfil} nombres={newDataRef.current} />*/}
+        <div className={show ? `container__popupzoom` : "container__popupzoom__"}>
           <PopupZoom perfil={getData.img} nombres={getData.nombres} />
 
-          <div className={false ? `container__popupzoom2` : "container__popupzoom__"}>
-            el alumno ya desayuno
+          <div
+            className={
+              getData.status === 1 || getData.status === 2
+                ? `container__popupzoom2`
+                : "container__popupzoom__"
+            }>
+            {getData.msg}
           </div>
         </div>
       </div>
