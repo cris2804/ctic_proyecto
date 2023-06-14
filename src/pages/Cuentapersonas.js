6e97cf19dcea320338f78871c8184bd500b1dd6f
HF-davis <63136307+HF-davis@users.noreply.github.com>
2023-06-14 12:14:59 -0500

    Update Cuentapersonas.js

--- src/pages/Cuentapersonas.js
+++ src/pages/Cuentapersonas.js
@@ -1,17 +1,16 @@
-import "./css/Cuentapersonas.css";
+import "./css/Conteo.css";
 import PopupZoom from "../components/PopupZoom";
 import Popup from "../components/Popup";
-//import data from "../assets/data";
-import { useEffect, useState, useRef } from "react";
-//import perfil from "../components/images/perfil.png";
-import io from "socket.io-client";
-import { Getip } from "../server/Getip";
+import { useEffect,useState,useRef } from "react";
+//import perfil from '../components/images/perfil.png';
 import Base64_jpg from "../components/Base64_jpg";
+import io from "socket.io-client";
+import {Getip} from '../server/Getip';
 
 
-const obtenerTurno = () => {
-  const fechaLocal = new Date();
-  const horaLocal = fechaLocal.getHours();
+const obtenerTurno = (horaLocal) => {
+  //const fechaLocal = new Date();
+  //const horaLocal = fechaLocal.getHours();
 
 
   if (horaLocal >= 7 && horaLocal <= 11) {
@@ -24,6 +23,8 @@ const obtenerTurno = () => {
     return 'Fuera de turno';
   }
 };
+
+
 
 
 export default function Cuentapersonas() {
@@ -44,31 +45,34 @@ export default function Cuentapersonas() {
     socket.on("rec_fac/rec_fac:rec_fac02", (data) => {
       if (!cargando) {
         newDataRef.current = data.nombres.value;
-
+        console.log(newDataRef.current);
         //const enviarDatos = () => {
-          const url = "localhost:5000";
+          const url = "http://localhost:5000/image";
+          console.log("antes de fetch")
 
           fetch(url, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({ nombres: newDataRef }),
+            body: JSON.stringify({ nombres: newDataRef.current}),
           })
             .then((response) => response.json())
             .then((data) => {
-
+              console.log("estoy en data")
+              ///////////
               setGetData({
                 nombres: data.nombres,
-                img: Base64_jpg(data.img),
+                img: require('../img/'+newDataRef.current+'.jpg'),//'Base64_jpg(data.img)',
               });
+              /////////////
               console.log(data);
             })
             .catch((error) => {
               console.error(error);
             });
         //};
-
+            console.log("sali de fetch")
         setShow(true);
         setCargando(true);
 
@@ -81,9 +85,13 @@ export default function Cuentapersonas() {
             ...prevData,
           ]);*/
           setDatactual((prevData) => [
-            getData,
+            {
+              nombres: newDataRef.current,
+              img: require('../img/'+newDataRef.current+'.jpg')
+            },
             ...prevData,
           ]);
+          
         }, 2000);
 
         return () => {
@@ -92,9 +100,9 @@ export default function Cuentapersonas() {
       }
     });
 
-    /*return () => {
+    return () => {
       socket.disconnect();
-    };*/
+    };
   }, [cargando, host, getData]);
 
   return (
@@ -114,7 +122,7 @@ export default function Cuentapersonas() {
       </div>
       <div className="container__right__cuenta__personas">
         {datactual.map((d, i) => {
-          return <Popup nombres={getData.nombres} perfil={getData.img} key={i} />;
+          return <Popup nombres={d.nombres} perfil={d.img} key={i} />;
         })}
 
         <div
