a70c2eed0d3aad3052d1fc631e92005f143667df
FranklinRiveraGranados <friverag@uni.pe>
2023-06-19 09:08:22 -0500

    Agregando botón en los poppups

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,32 +1,13 @@
-/*import './css/Popup.css';
-
-export default function PopupZoom({firstname, lastname, perfil}){
-    return(
-        <div className="container__popup">
-            <div className='container__top2'>
-                <img src={perfil} alt='ima-perfil'/>
-            </div>
-            <div className='container__bottom2'>
-                {firstname} <br></br>
-                {lastname}
-            </div>
-        </div>
-    )
-}*/
-
 import './css/Popup.css';
-import { RiCloseCircleFill } from "react-icons/ri";
-
 export default function PopupZoom({nombres, perfil}){
     return(
-        <div className="container__popup">
+        <div>
             <div className='container__top2'>
                 <img src={perfil} alt='ima-perfil'/>
             </div>
             <div className='container__bottom2'>
                 {nombres}
             </div>
-            <div className='container__close'><RiCloseCircleFill className="close"  /></div>
         </div>
     )
 }
--- src/pages/Cuentapersonas.js
+++ src/pages/Cuentapersonas.js
@@ -1,14 +1,12 @@
 import "./css/Cuentapersonas.css";
 import PopupZoom from "../components/PopupZoom";
 import Popup from "../components/Popup";
-import { useEffect, useState, useRef } from "react";
-//import perfil from '../components/images/perfil.png';
+import { useEffect, useState } from "react";
+import { RiCloseCircleFill } from "react-icons/ri";
 import io from "socket.io-client";
-import { Getip } from "../server/Getip";
+//import { Getip } from "../server/Getip";
 
 const obtenerTurno = (horaLocal) => {
-  //const fechaLocal = new Date();
-  //const horaLocal = fechaLocal.getHours();
 
   if (horaLocal >= 7 && horaLocal <= 11) {
     return "Turno desayuno";
@@ -24,11 +22,7 @@ const obtenerTurno = (horaLocal) => {
 const host = "http://192.168.52.232:9090";
 let interval = null;
 export default function Cuentapersonas() {
-  const [show, setShow] = useState(true);
   const [datactual, setDatactual] = useState([]);
-  const [count, setCount] = useState(0);
-  const [cargando, setCargando] = useState(false);
-  const newDataRef = useRef(null);
   const [getData, setGetData] = useState({ nombres: "", img: "",show:false });
   useEffect(()=>{
     const socket = io(host, {
@@ -59,13 +53,6 @@ export default function Cuentapersonas() {
         show:true,
         img:`./imagenes/${dataJson.nombres}.jpg`
       });
-       
-
-      /*setGetData({
-        nombres:"Hola"+datactual.length +1,
-        status: 1,
-        show:true
-      });*/
     });
     return () =>{
       socket.disconnect();
@@ -78,7 +65,7 @@ export default function Cuentapersonas() {
         dataUpdate.show = false;
         
         setGetData(dataUpdate);
-        if(dataUpdate.status != 0) return 
+        if(dataUpdate.status !== 0) return 
         setDatactual((prev)=>[
           dataUpdate,...prev
         ])
@@ -102,7 +89,10 @@ export default function Cuentapersonas() {
       </div>
       <div className="container__right__cuenta__personas">
         {datactual.map((d, i) => {
-          return <Popup nombres={d.nombres} perfil={d.img} key={i} />;
+          return <div className='container__popup'>
+                    <Popup nombres={d.nombres} perfil={d.img}/>
+                    <div className='container__close'><RiCloseCircleFill className="close"  /></div>
+                  </div>;
         })}
 
         <div className={getData.show ? `container__popupzoom` : "container__popupzoom__"}>
@@ -121,60 +111,3 @@ export default function Cuentapersonas() {
     </div>
   );
 }
-/*
-const url = "http://localhost:5000/image";
-      const dataFetch = await fetch(url,{
-        method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ nombres: dataName })
-      })
-      const dataJson = await dataFetch.json();
-      setGetData({
-        nombres: dataJson.nombres,
-        msg: dataJson.msg,
-        status: dataJson.status
-      });
-*/
-/*
-  
-  useEffect(()=>{
-    
-    socket.on("rec_fac/rec_fac:rec_fac02"), async (data)=>{
-      const dataName = data.nombres.value;
-      newDataRef.current = data.nombres;
-
-      
-      setShow(true);
-      setCargando(true);
-    }
-    return () => {
-      socket.disconnect();
-    };
-  },[]);
-  useEffect(()=>{
-    let intervalo;
-    if(getData?.status !== 0){
-      intervalo = setTimeout(()=>{
-        setShow(false);
-        setCargando(false);
-        setCount((prevCount) => prevCount + 1);
-        if (getData.status === 0) {
-          setDatactual((prevData) => [
-            {
-              nombres: getData.nombres,
-              //img: require('../img/'+newDataRef.current+'.jpg')
-              img: require("../components/images/perfil.png"),
-            },
-            ...prevData,
-          ]);
-        }
-      },2000)
-    }
-    return () =>{
-      clearInterval(intervalo);
-    }
-  },[getData])
-
-*/
