01f67bb6ac1fbaeb3604a3863d556724fa50a093
FranklinRiveraGranados <friverag@uni.pe>
2023-07-06 11:29:32 -0500

    Agregando gráfica para calidad del aire exteriores

---
+++ src/components/calidadAireExterior/Grafico.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Label,
+  ReferenceArea,
+} from "recharts";
+import "../css/Grafico.css";
+
+const Grafico = ({ nombre, id }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const CustomDot = ({ cx, cy, stroke, fill, payload, value }) => {
+    if (value <= 800 && value > 0) {
+      return (
+        <svg
+          x={cx - 5}
+          y={cy - 5}
+          width={10}
+          height={10}
+          fill="#9AD64D"
+          viewBox="0 0 10 10"
+        >
+          <circle cx={5} cy={5} r={5} fill="#9AD64D" />
+        </svg>
+      );
+    } else if (value <= 1000 && value > 800) {
+      return (
+        <svg
+          x={cx - 5}
+          y={cy - 5}
+          width={10}
+          height={10}
+          fill="#F8CD38"
+          viewBox="0 0 10 10"
+        >
+          <circle cx={5} cy={5} r={5} fill="#F8CD38" />
+        </svg>
+      );
+    } else if (value > 1000) {
+      return (
+        <svg
+          x={cx - 5}
+          y={cy - 5}
+          width={10}
+          height={10}
+          fill="#FF4242"
+          viewBox="0 0 10 10"
+        >
+          <circle cx={5} cy={5} r={5} fill="#FF4242" />
+        </svg>
+      );
+    }
+  };
+
+  console.log(id,nombre)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          //`http://192.168.52.232:9090/api/v1/carga-viral/${id}?last=20`
+          `http://192.168.52.232:9090/api/v1/calidad-de-aire/${id}?last=10`
+        );
+        const data = await response.json();
+        const newData = data.map((item) => {
+          const time = new Date(item.time_index);
+          //const hourMinuteSecond = time.toISOString().substring(11, 19); // Captura la parte de la cadena con hora:minuto:segundo
+          const options = {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false,
+          };
+          const hourMinuteSecond = time.toLocaleTimeString([], options);
+
+          let val = 0;
+          //if (nombre === "Co2") val = item.dioxido_de_carbono;
+          if (nombre === "PM2.5") val = item.calidad_de_aire;
+          else if (nombre === "PM 10") val = item.dioxido_de_carbono;
+          else if (nombre === "O3") val = item.ozono;
+          else if (nombre === "NO2") val = item.dioxido_de_nitrogeno;
+          else if (nombre === "H2S") val = item.sulfuro_de_hidrogeno;
+          else if (nombre === "CO") val = item.monoxido_de_carbono;
+          return {
+            dioxido_de_carbono: val,
+            time_index: hourMinuteSecond,
+          };
+        });
+
+        setData(newData);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+    const interval = setInterval(fetchData, 60000); // Llama a la función fetchData cada 8 minutos
+
+    // Limpia el intervalo al desmontar el componente
+    return () => {
+      clearInterval(interval);
+    };
+  }, [id, nombre]);
+  if (loading) {
+    return <div>Cargando...</div>; // O cualquier indicador de carga que desees mostrar
+  }
+
+  return (
+    <div className="grafico">
+      <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart
+          width={300}
+          height={300}
+          data={data}
+          margin={{
+            top: 15,
+            right: 30,
+            left: 20,
+            bottom: 25,
+          }}
+        >
+          <CartesianGrid stroke="#eee" />
+          <XAxis dataKey="time_index" reversed={true}>
+            <Label value="Tiempo" offset={0} position="bottom" fill="#000" />
+          </XAxis>
+          <YAxis domain={[0, 60]}>
+            <Label
+              value={`${nombre}( µg/m³ )`}
+              offset={5}
+              angle={-90}
+              position="insideLeft"
+              fill="#000"
+              text
+            />
+          </YAxis>
+          <Line
+            type="monotone"
+            dataKey="dioxido_de_carbono"
+            stroke="#000"
+            strokeWidth={0.5}
+            dot={<CustomDot />}
+            isAnimationActive={false}
+          />
+          <ReferenceArea y1={0} y2={800} fill="green" fillOpacity={0.2} />
+          <ReferenceArea y1={800} y2={1000} fill="orange" fillOpacity={0.2} />
+          <ReferenceArea y1={1000} y2={1600} fill="red" fillOpacity={0.2} />
+          <Tooltip />
+          {/*<Legend />*/}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Grafico;
--- src/pages/DetalleCA.js
+++ src/pages/DetalleCA.js
@@ -2,7 +2,7 @@ import "./css/DetalleCA.css";
 import happy from "../components/images/happy.png";
 import { useState } from "react";
 import { Ica } from "../assets/Ica";
-//import Grafico from "../components/Grafico";
+import Grafico from "../components/calidadAireExterior/Grafico";
 import Grafico2 from "../components/Grafico2";
 import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import Calendar from "react-calendar";
@@ -27,7 +27,7 @@ const gases = [
   },
   {
     key: 3,
-    nombre: "SO2",
+    nombre: "O3",
   },
   {
     key: 4,
@@ -39,7 +39,7 @@ const gases = [
   },
   {
     key: 6,
-    nombre: "NO",
+    nombre: "CO",
   },
 ];
 
@@ -49,7 +49,7 @@ function obtenerNombre(id) {
   else if (id === "puerta 5") return "puerta 5";
 }
 
-function Graficar(opc, nom) {
+function Graficar(opc, nom, idb) {
   const datos = Array.from({ length: 20 }, () => {
     return {
       time: new Date().toLocaleTimeString(),
@@ -57,7 +57,7 @@ function Graficar(opc, nom) {
     };
   });
   if (opc === 0) {
-    return <Grafico2 datos={datos} nombre={nom} />;
+    return <Grafico id={idb} nombre={nom} />;
   } else if (opc === 1) {
     return <Grafico2 cantidad={96} nombre={nom} />;
   } else if (opc === 2) {
@@ -563,7 +563,7 @@ export default function DetalleCA() {
                             <div className={seleccionado === 1 ? 'tiempo__seleccionado':''} onClick={() => Seleccionar(1)}>DÍA</div>
                             <div className={seleccionado === 2 ? 'tiempo__seleccionado':''} onClick={() => Seleccionar(2)}>SEMANA</div>
                         </div>*/}
-            <div className="container__grafico">{Graficar(0, opcion)}</div>
+            <div className="container__grafico">{Graficar(0, opcion, retornaidb(id))}</div>
 
             <div className="container__contaminante__principal">
               <div className="container__c__p">
