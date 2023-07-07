32505bfcc10a6957f140f9970ca383bb9b4f2da9
FranklinRiveraGranados <friverag@uni.pe>
2023-07-07 17:22:00 -0500

    Agregando hora y fecha

--- src/components/obtenerfecha.js
+++ src/components/obtenerfecha.js
@@ -1,7 +1,8 @@
-export const obtenerfecha = () => {
+const obtenerfecha = () => {
   const fechaActual = new Date();
   const diaActual = fechaActual.getDate();
   const mesActual = fechaActual.getMonth() + 1; // Los meses en JavaScript empiezan en 0, por lo que debemos agregar 1
+  
   let mes = "";
 
   switch (mesActual) {
@@ -47,3 +48,24 @@ export const obtenerfecha = () => {
 
   return `${mes} ${diaActual}`;
 };
+
+const fechaFormato2 = () => {
+  const fechaActual = new Date();
+  const diaActual = fechaActual.getDate();
+  const mesActual = fechaActual.getMonth() + 1; // Los meses en JavaScript empiezan en 0, por lo que debemos agregar 1
+  const anioActual = fechaActual.getFullYear();
+
+  let dia = ""
+  let mes = ""
+
+  if(diaActual < 10) dia = "0"+diaActual
+  else dia = diaActual
+  
+  if(mesActual < 10) mes = "0"+mesActual
+  else mes = mesActual
+
+  return `${dia}/${mes}/${anioActual}`;
+}
+
+
+export {obtenerfecha, fechaFormato2}
--- src/components/senasa/Popup.js
+++ src/components/senasa/Popup.js
@@ -1,4 +1,6 @@
 import "./Popup.css"
+import { fechaFormato2 } from "../obtenerfecha"
+import { obtenerhora } from "../obtenerhora"
 
 import iconoMosca from "../../pages/images/icono-mosca.png"
 
@@ -7,20 +9,20 @@ function Popup(){
     return(
         <div className="container__component__popup">
             <div className="container__senasa__titulo">
-                Trampa 1
+                TRAMPA 1
             </div>
             <div className="container__depar__pais">
                 Junín, Perú
             </div>
             <div className="container__fecha__hora">
-                06/06/2023 16:24
+                {fechaFormato2()}, {obtenerhora()}
             </div>
             <div className="container__img__cant__moscas__">
                 <div className="container__img__cant__moscas__2">
                     <div className="container__imagen__popup__senasa"><img src={iconoMosca} alt="" /></div>
                     <div className="container__cantidad__moscas__popup">
                         <span>80</span>
-                        <span>moscas</span>
+                        <span>Moscas</span>
                     </div>
                 </div>
             </div>
