fd178b167be56628d21bb2f355b2b821003669f3
FranklinRiveraGranados <friverag@uni.pe>
2023-07-14 16:58:58 -0500

    separando gases

---
+++ src/assets/gases.js
@@ -0,0 +1,36 @@
+const gases = [
+    {
+      key: 1,
+      nombre: "PM2.5",
+    },
+    {
+      key: 2,
+      nombre: "PM 10",
+    },
+    {
+      key: 3,
+      nombre: "O3",
+    },
+    {
+      key: 4,
+      nombre: "NO2",
+    },
+    {
+      key: 5,
+      nombre: "H2S",
+    },
+    {
+      key: 6,
+      nombre: "CO",
+    },
+    {
+      key: 7,
+      nombre: "NO",
+    },
+    {
+      key: 8,
+      nombre: "SO2"
+    }
+  ];
+
+export default gases
--- src/pages/DetalleCA.js
+++ src/pages/DetalleCA.js
@@ -16,41 +16,7 @@ import { MdOutlineSportsHandball } from "react-icons/md";
 import { MdLocationPin } from "react-icons/md";
 import { Getip } from "../server/Getip";
 import unidaddv from "../assets/unidaddv";
-
-const gases = [
-  {
-    key: 1,
-    nombre: "PM2.5",
-  },
-  {
-    key: 2,
-    nombre: "PM 10",
-  },
-  {
-    key: 3,
-    nombre: "O3",
-  },
-  {
-    key: 4,
-    nombre: "NO2",
-  },
-  {
-    key: 5,
-    nombre: "H2S",
-  },
-  {
-    key: 6,
-    nombre: "CO",
-  },
-  {
-    key: 7,
-    nombre: "NO",
-  },
-  {
-    key: 8,
-    nombre: "SO2"
-  }
-];
+import gases from "../assets/gases";
 
 function obtenerNombre(id) {
   if (id === "ctic") return "ctic";
