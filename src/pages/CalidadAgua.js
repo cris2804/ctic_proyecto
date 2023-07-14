038f2c02a92dfd4434d96a2c07a4e04c6ac8c584
Royer Lopez Flores <72278068+RoyerLopezFlores@users.noreply.github.com>
2023-07-13 20:53:51 -0500

    Modificando primera carga Calidad de Agua

--- src/pages/CalidadAgua.js
+++ src/pages/CalidadAgua.js
@@ -2,19 +2,32 @@ import "./css/CalidadAgua.css";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import { Getip } from "../server/Getip";
+//import { async } from "q";
 
 //const host = "http://192.168.52.232:9090";
+
 export default function CalidadAgua(){
     // para obtener el id de la ruta donde nos encontramos
     const searchParams = new URLSearchParams(window.location.search);
     const id = searchParams.get("id");
     let host = window.location.host; //para obtener la ip
     const [datactual, setDatactual] = useState(null)
-
+    const fetchData = async () =>{
+        //Getip(host)
+        const url = "http://localhost:3003/";
+        const data = await fetch(`${url}api/v1/calidad-de-agua?last=1&columns=00000111`);
+        const dataJson = await data.json();
+        setDatactual({
+            temperatura:{value:dataJson[0].temperatura},
+            ph:{value:dataJson[0].ph},
+            turbidez:{value:dataJson[0].turbidez},
+        });
+    }
     useEffect(()=>{
         const socket = io(Getip(host), {
           transports: ["websocket"],
         });
+        fetchData();
         socket.on("calidad_agua/calidad_agua:rak-3272s-o",async (data)=>{
                   
           setDatactual(data)
@@ -41,7 +54,7 @@ export default function CalidadAgua(){
                     <div>
                         Temperatura
                     </div>
-                    <div className="valor">
+                    <div className={"valor"}>
                         {datactual ? datactual.temperatura.value : ""} °C
                     </div>
                 </div>
