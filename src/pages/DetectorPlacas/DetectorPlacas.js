8029fbe4931cefc3187d752724ab547901b47c17
FranklinRiveraGranados <friverag@uni.pe>
2023-07-27 11:49:52 -0500

    Agregando cosas

--- src/pages/DetectorPlacas/DetectorPlacas.js
+++ src/pages/DetectorPlacas/DetectorPlacas.js
@@ -1,10 +1,14 @@
 import "./DetectorPlacas.css"
 import placa from "./placa.jpg"
 import Tabla from "./Tabla";
+import { useState } from "react";
 //import fondocarro from "./fondo-carro.png"
 
 
 export default function DetectorPlacas() {
+
+  const [option, setOption] = useState(1);
+
   return (
     <div className="container__main__detector__placas">
       <div className="container__main__placas">
@@ -28,7 +32,7 @@ export default function DetectorPlacas() {
             {/*<img src={fondocarro} alt=""/>*/}
             <div className="container__ingreso__salida">
                 <div>Ingreso: </div>
-                <div>1500</div>
+                <div>50</div>
             </div>
             <div className="container__ingreso__salida">
                 <div>Salida: </div>
@@ -37,7 +41,11 @@ export default function DetectorPlacas() {
         </div>
 
         <div className="container__placa container__grafica__carros">
-            
+            <div className="container__opc__button">
+              <div onClick={()=>setOption(1)} className={option === 1 ? "selected" : ""}>Entrados</div>
+              <div onClick={()=>setOption(2)} className={option === 2 ? "selected" : ""}>Salidos</div>
+              <span className={option === 1 ? "selected1" : "selected2"}></span>
+            </div>
 
         </div>
       </div>
