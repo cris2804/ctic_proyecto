e54009c48a05733dbc9373584a03ebc352fa548f
FranklinRiveraGranados <friverag@uni.pe>
2023-08-02 18:57:11 -0500

    Agregando cambios en sp

--- src/pages/SmartParking/SmartParking.js
+++ src/pages/SmartParking/SmartParking.js
@@ -1,9 +1,50 @@
 import "./SmartParking.css"
+import { MapContainer, TileLayer } from "react-leaflet"
+import {ubi} from "./ubicaciones"
+import { Marker } from "react-leaflet"
+import L from "leaflet"
 
-export default function SmartParking(){
-    return(
-        <div className="contianer__all__sp">
-            Smart Parking
+//const ubiCentro = ["-12.020381", "-77.049178"];
+const ubiCentro = [-12.016460,-77.049896]
+
+function SmartParking(){
+
+    const handleMostrar = (e, i) => {
+        //setId(e);
+        //setBol(true);
+        //setI(i);
+        console.log(e,i)
+      };
+
+    return (
+        <div className="container__all__senasa">
+            <div className="container__mapa__sp">
+                <MapContainer center={ubiCentro} zoom={18}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {ubi.map((ubicacion) => {
+                    return (
+                    <Marker
+                        key={ubicacion.clave}
+                        position={ubicacion.coordenadas}
+                        icon={L.icon({
+                        iconUrl: ubicacion.logo,
+                        iconRetinaUrl: ubicacion.logo,
+                        iconSize: [40, 40],
+                        className: "leaflet-venue-icon",
+                        })}
+                        eventHandlers={{
+                        click: () => handleMostrar(ubicacion.clave, ubicacion.i),
+                        }}
+                    />
+                    );
+                })}
+                </MapContainer>
+            </div>
         </div>
     )
 }
+
+export default SmartParking
---
+++ src/pages/SmartParking/ubicaciones.js
@@ -0,0 +1,18 @@
+import Icon from "../../assets/icon.svg";
+
+export const ubi = [
+    {
+        "clave": "sp-ctic",
+        "nombre": "CTIC",
+        "coordenadas": [-12.016460,-77.049896],
+        "logo": Icon,
+        "i": 1,
+    },
+    /*{
+        "clave": "cv-comedor",
+        "nombre": "Comedor Universitario",
+        "coordenadas": [-12.018866,-77.049183],
+        "logo": Icon,
+        "i": 1,
+    }*/
+]
