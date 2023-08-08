aa5c67f97d461bb97fadbc08f183a71bf9092b85
Royer Lopez Flores <72278068+RoyerLopezFlores@users.noreply.github.com>
2023-08-08 12:34:23 -0500

    actualizando popup sp

--- src/pages/SmartParking/ControllerSmartParking.js
+++ src/pages/SmartParking/ControllerSmartParking.js
@@ -1,83 +1,114 @@
 class ControllerSmartParking{
-    constructor({contenedor,src,estado_inicial,props}){
+    constructor({id,contenedor,src,cantidad,estado_inicial,props}){
+        this.id = id;
         this.contenedor = contenedor;
         this.padre = contenedor.parentNode;
         this.src = src;
         this.props = props;
         this.scala = 1;
         this.estado_inicial = estado_inicial || [];
+        this.cantidad = cantidad || 0;
         this.iniciar();
-        this.chargeImage();
-    }
-    iniciar(){
-        const canvas = document.createElement("canvas");
-        canvas.className ="canvas-container-temp"
-        const {width,height} = this.padre.getBoundingClientRect();
         
+    }
+    async iniciar(){
+        console.log(this.contenedor.getBoundingClientRect(),this.padre.getBoundingClientRect());
+        let canvas = null;
+        if(document.getElementById(this.id)){
+            canvas = document.getElementById(this.id);
+        }else{
+            canvas = document.createElement("canvas");
+            canvas.id = this.id;
+            canvas.className ="canvas-container-temp"
+        }
+        
+        
+        const {width,height} = this.padre.getBoundingClientRect();
         canvas.width = width*0.8;
         canvas.height = height*0.9;
-        
-        
         this.contenedor.appendChild(canvas);
         this.width = width*0.8;
         this.height = height*0.9;
         this.canvas = canvas;
         const context = canvas.getContext("2d");
         this.context = context;
+        await this.chargeImage();
+        
     }
-    
-    chargeImage(){
-        const image = new Image();
-        image.onload = () =>{
-            const wi = image.width;
-            const hi = image.height;
+    graficarFondo(image){
+        const wi = image.width;
+        const hi = image.height;
 
 
-            const dw = wi - this.width;
-            const dh = hi - this.height;
-            const sw = this.width/wi;
-            const sh = this.height/hi;
-            if(dw < 0 || dh < 0){
-                const scala = Math.min(sw, sh);
-                const nw = wi*scala;
-                const nh = hi*scala;
-                console.log(nw,hi)
+        const dw = wi - this.width;
+        const dh = hi - this.height;
+        const sw = this.width/wi;
+        const sh = this.height/hi;
+        if(dw < 0 || dh < 0){
+            const scala = Math.min(sw, sh);
+            const nw = wi*scala;
+            const nh = hi*scala;
+            console.log(nw,hi)
 
-                this.scala = scala;
-                this.contenedor.style.width = `${nw}px`;
-                this.contenedor.style.height = `${nh}px`;
-                this.canvas.width =nw;
-                this.canvas.height = nh;
+            this.scala = scala;
+            this.contenedor.style.width = `${nw}px`;
+            this.contenedor.style.height = `${nh}px`;
+            this.canvas.width =nw;
+            this.canvas.height = nh;
 
-                this.context.drawImage(image, 0,0,wi,hi,0,0,wi*scala,hi*scala);
-                this.dw = 0;
-                this.dh = 0;
-                //this.iniciarTR();
-                this.setArrayState(this.estado_inicial);
-                
-            }else{
-                console.log(dw,dh);
-
-                this.dw = dw;
-                this.dh = dh;
-                
-                
-                this.context.drawImage(image, 0,0,wi-Math.min(dw*0.5,0),hi-Math.min(0,dw*0.5),-dw*0.5,-dh*0.5,wi,hi);
-                //this.iniciarTR();
-                this.setArrayState(this.estado_inicial);
-            }
+            this.context.drawImage(image, 0,0,wi,hi,0,0,wi*scala,hi*scala);
+            this.dw = 0;
+            this.dh = 0;
             
+            //this.iniciarTR();
+            //this.setArrayState(this.estado_inicial);
+            
+        }else{
+            console.log(dw,dh);
+
+            this.dw = dw;
+            this.dh = dh;
+            
+            
+            this.context.drawImage(image, 0,0,wi-Math.min(dw*0.5,0),hi-Math.min(0,dw*0.5),-dw*0.5,-dh*0.5,wi,hi);
+            //this.iniciarTR();
+            //this.setArrayState(this.estado_inicial);
         }
-        image.src = this.src;
+    }
+    chargeImage(){
+        console.log(this.image);
+        if(this.image){
+            console.log("holaXD");
+            //this.graficarFondo(this.image);
+            return;
+        }
+        return new Promise((resolve,reject)=>{
+            const image = new Image();
+            image.onload = () =>{
+                this.graficarFondo(image)
+                this.image = image;
+                this.activo = true;
+                this.iniciarTR();
+                resolve();
+            };
+            image.onerror = (error) => {
+                reject(error); // Rechazamos la promesa en caso de error al cargar la imagen
+            };
+            image.src = this.src;
+        })
+            
     }
     resizeContainer(){
 
     }
-    iniciarTR(){
+    async iniciarTR(){
         console.log("Escala: ", this.scala);
-        const n = 13;
+        const n = this.cantidad;
+        //this.context.fillRect(0,0,100,100);
+        //console.log(this.context);
         for(let i = 0;i<n;i++){
-            let color = Math.random()>0.5? "#c3e7ff":"#fa8071";
+            //let color = Math.random()>0.5? "#c3e7ff":"#fa8071";
+            const color = "#fa8071";
             this.context.fillStyle = color;
             const x = (435-this.dw*0.5 + i*20)*this.scala;
             const y = (400-this.dh*0.5)*this.scala;
@@ -87,10 +118,18 @@ class ControllerSmartParking{
             
         }
     }
-    setArrayState(array){
-        const n = 13;
-        console.log("hola",array,this.scala);
-        for(let i = 0;i<n;i++){
+    async setArrayState(array){
+        //console.log(this.promesa);
+        await this.chargeImage();
+
+        //        this.graficarFondo(this.image);
+        
+        //console.log("hola",array,this.scala,"XD",this.activo);
+        this.fillStyle = "blue";
+        //this.context.fillRect(0,0,100,100);
+        //console.log(this.context);
+        for(let i = 0;i<Math.min(array.length,this.cantidad);i++){
+            console.log("XD1",array[i]);
             let color = (array[i]==0? "#c3e7ff":"#fa8071");
             this.context.fillStyle = color;
             const x = (435-this.dw*0.5 + i*20)*this.scala;
--- src/pages/SmartParking/RealTimeComponentSmartParking.js
+++ src/pages/SmartParking/RealTimeComponentSmartParking.js
@@ -1,50 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SmartParking.css";
 import { useRef } from 'react';
 import { useEffect } from 'react';
 import { ControllerSmartParking } from './ControllerSmartParking';
 import io from "socket.io-client"
 import { Getip } from "../../server/Getip"
-export default function RealTimeComponentSmartParking() {
+export default function RealTimeComponentSmartParking({visible}) {
     const container = useRef(null);
+    const [hileras,setHileras] = useState({});
+    const [controller,setController] = useState(null);
     const searchParams = new URLSearchParams(window.location.search);
     const id = searchParams.get("id");
     let host = window.location.host; 
 
     useEffect(()=>{
-        if(!container) return;
-        const contenedor = container.current;
-        const controllerSP1 = new ControllerSmartParking(
-            {
-                contenedor:contenedor,
-                src :"./SmartParking/SmartParkingCTICFrontis.png" ,
-            }
-        )   
-        let  i = 0;
         const socket = io(Getip(host), {
             transports: ["websocket"],
           });
           //fetchData();
-          
-          socket.on("smartparking_va/smartparking_va:ctic",async (data)=>{
-            const controllerSP = new ControllerSmartParking(
-                {
-                    contenedor:contenedor,
-                    src :"./SmartParking/SmartParkingCTICFrontis.png" ,
-                    estado_inicial: data.estado.value
-                }
-            )       
-            controllerSP.setArrayState(data.estado.value);
-            console.log(data.estado.value)
+        socket.on("smartparking_va/smartparking_va:ctic",async (data)=>{
             
-          });
-          return () =>{
+            setHileras([0,1,1,0,0,0]);
+            const h  = data.estado.value.split("");
+            setHileras(h.map(e => parseInt(e)));
+            
+        });
+        
+        return () =>{
             socket.disconnect();
-          };
-    },[])
+        };
+        
+    },[]);
+    const actualizarHilera = async( array )=>{
+        if(!controller) return;
+        await controller.setArrayState(array);
+    }
+    const changeHilera = () =>{
+        setHileras(Array.from({length:10}, () => (Math.random()>0.5?0:1)));
+    }
+    useEffect(()=>{
+        //console.log(visible);
+        if(!controller && visible){
+            console.log("creando");
+            const cont = new ControllerSmartParking({
+                id:"parking-h1",
+                contenedor:container.current,
+                src: "./SmartParking/SmartParkingCTICFrontis.png",
+                cantidad: 13
+            });
+            setController(cont)
+            cont.setArrayState(hileras);
+            return;
+        }
+        actualizarHilera(hileras);
+        
+    },[hileras,visible])
     return (
     <div ref={container} className='cont-smart-parking-tr'>
-        <div className='leyenda-popup-hilos'>
+        <div className='leyenda-popup-hilos' onClick={changeHilera}>
             <div className='item-legend color-disponible'></div>
             <div>Espacio libre</div>
             <div className='item-legend color-ocupado'></div>
