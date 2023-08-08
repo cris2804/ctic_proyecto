a6a83c4a14e0b18a0148833ca3232c945d9836d2
FranklinRiveraGranados <friverag@uni.pe>
2023-08-08 09:35:55 -0500

    Agregando cambios en dp

--- src/pages/DetectorPlacas/DetectorPlacas.js
+++ src/pages/DetectorPlacas/DetectorPlacas.js
@@ -1,68 +1,52 @@
 import "./DetectorPlacas.css"
+import imagen from "./carro.png"
 import placa from "./placa.jpg"
-import Tabla from "./Tabla";
-import { useState } from "react";
-import carro from "./carro.png"
 
+export default function DetectorPlacas(){
 
-export default function DetectorPlacas() {
-
-  const [option, setOption] = useState(1);
-
-  return (
-    <div className="container__main__detector__placas">
-      <div className="container__main__placas">
-        <div className="container__tabla__title container__placa">
-            <div>Car Analytics: Puerta 6 Camara 2</div>
-          <div className="container__tabla"><Tabla/></div>
-        </div>
-
-        <div className="container__placa container__imagen__placas">
-            <div className="container__img__principal">
-                <div className="container__img__ultimo">
-                  <div>11:52:20 AM</div>
-                  <img src={placa} alt=""/>
+    return(
+        <div className="container__main__all__dp">
+            <div className="container__left__dp">
+                <div className="container__img"><img src={imagen} /></div>
+                <div className="container__cantidad"> 
+                    <div className="container__is__dp">
+                        <div>Ingreso</div>
+                        <div>620</div>
+                    </div>
+                    <div className="container__is__dp">
+                        <div>Salida</div>
+                        <div>20</div>
+                    </div>
                 </div>
             </div>
-            <div className="container__imgs__secundarios">
-              <div className="container__img__secundario">
-                <div>11:48:35 AM</div>
-                <img src={placa} alt=""/>
-              </div>
-              <div className="container__img__secundario">
-                <div>11:47:16 AM</div>
-                <img src={placa} alt=""/>
-              </div>
-              <div className="container__img__secundario">
-                <div>11:20:56 AM</div>
-                <img src={placa} alt=""/>
-              </div>
+            <div className="container__right__dp">
+                <div className="container__main__hi__dp">
+                    {/*<div className="container__ultimo">Último carro ingresado (registrado)</div>*/}
+                    <div className="container__hora__img_dp">
+                        <div>12:45:20 PM</div>
+                        <img src={placa}/>
+                        <div>Último carro que ingreso</div>
+                    </div>
+                </div>
+                <div className="container__main__form__dp">
+                    <div className="container__form__dp">
+                        <div className="container__title__form__dp">Registrar Carro</div>
+                        <div>
+                            <div>Placa</div>
+                            <input />
+                        </div>
+                        <div>
+                            <div>Nombre del conductor</div>
+                            <input />
+                        </div>
+                        <div>
+                            <div>Dni</div>
+                            <input />
+                        </div>
+                        <div className="container__btn__registrar__dp">REGISTRAR</div>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div className="container__placa container__cantidad__carros">
-            <img src={carro} alt=""/>
-            <div className="container__main__is">
-              <div className="container__ingreso__salida">
-                  <div>Ingreso: </div>
-                  <div>50</div>
-              </div>
-              <div className="container__ingreso__salida">
-                  <div>Salida: </div>
-                  <div>30</div>
-              </div>
-            </div>
-        </div>
-
-        <div className="container__placa container__grafica__carros">
-            <div className="container__opc__button">
-              <div onClick={()=>setOption(1)} className={option === 1 ? "selected" : ""}>Entrados</div>
-              <div onClick={()=>setOption(2)} className={option === 2 ? "selected" : ""}>Salidos</div>
-              <span className={option === 1 ? "selected1" : "selected2"}></span>
-            </div>
-
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
