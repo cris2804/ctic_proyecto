3be4aba0a7fea59ebf509f32b7903bc1fc4ee8ae
FranklinRiveraGranados <friverag@uni.pe>
2023-08-23 14:39:00 -0500

    cambios

--- src/pages/CalidadAire/CalidadAire.js
+++ src/pages/CalidadAire/CalidadAire.js
@@ -1,20 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./pages/CalidadAire/Main/Main";
 import DetalleCA from "./pages/CalidadAire/Exteriores/DetalleCA";
-import Grafico3 from "./components/Grafico3";
-import Landing from "./pages/LandingPage/Landing";
-import ReconocimientoFacial from "./pages/Test/ReconocimientoFacial";
-import CanvasComedor from "./components/comedor/CanvasComedor";
-import Cuentaspersonas from "./pages/Test/Cuentapersonas";
-import Senasa from "./pages/Senasa/Senasa";
-import CalidadAgua from "./pages/CalidadAgua/CalidadAgua";
-import ControlAforo from "./pages/CuentaPersonas/ControlAforo";
-import Metaverso from "./pages/Metaverso/Metaverso";
-import DetectorPlacas from "./pages/DetectorPlacas/DetectorPlacas";
-import SmartParking from "./pages/SmartParking/SmartParking";
-import CalidadAire from "./pages/CalidadAire/CalidadAire";
+import FaqCAI from "./pages/CalidadAire/Interiores/FaqCAI";
+import FaqCAE from "./pages/CalidadAire/Exteriores/FaqCAE";
+import DetalleCAI from "./pages/CalidadAire/Interiores/DetalleCAI";
+import ComedorCAI from "./pages/CalidadAire/Interiores/ComedorCAI";
+import Bienvenida from "./pages/CalidadAire/Bienvenida/Bienvenida";
 
-function App() {
+function CalidadAire() {
 
   return (
     <div>
@@ -121,39 +115,17 @@ function App() {
       <div>
         <Router>
           <Routes>
-            <Route path="/" element={<Landing/>}/>
-            {/*
+            <Route path="/calidad-del-aire" element={<Bienvenida/>}/>
+            
             <Route path="/calidad-del-aire-uni" element={<Main />} />
             
             <Route path="/calidad-del-aire-exteriores" element={<DetalleCA />}/>
+            <Route path="/faq-calidad-de-aire" element={<FaqCAE />} />
             
             <Route path="/calidad-del-aire-interiores-ctic" element={<DetalleCAI />}/>
             <Route path="/calidad-del-aire-interiores-comedor-universitario" element={<ComedorCAI />}/>
-            
             <Route path="/faq-carga-viral" element={<FaqCAI />} />
-            <Route path="/faq-calidad-de-aire" element={<FaqCAE />} />*/}
 
-            <Route path="/calidad-del-aire" element={<CalidadAire/>} />
-            
-            <Route path="/cuenta-personas" element={<ReconocimientoFacial/>} />
-            {/*<Route path="/control-aforo" element={<Controlaforo/>}/>*/}
-            <Route path="/reconocimiento-facial" element={<Cuentaspersonas/>} />
-
-            <Route path="/senasa" element={<Senasa/>} />{/*}
-            <Route path="/calidad-del-aire" element={<Bienvenida/>}/>*/}
-            <Route path="/calidad-del-agua" element={<CalidadAgua/>}/>
-
-
-            <Route path="/temporal" element={<CanvasComedor/>}/>
-            <Route path="/detalle-carga-viral" element={<DetalleCA />} />
-            <Route path="/grafico-semana" element={<Grafico3 />} />
-            <Route path="/control-aforo" element={<ControlAforo/>} />
-            <Route path="/metaverso" element={<Metaverso/>} />
-
-
-
-            <Route path="/detector-de-placas" element={<DetectorPlacas/>}/>
-            <Route path="/smart-parking" element={<SmartParking/>}/>
           </Routes>
         </Router>
       </div>
@@ -161,4 +133,4 @@ function App() {
   );
 }
 
-export default App;
+export default CalidadAire;
--- src/pages/CalidadAire/Main/Main.js
+++ src/pages/CalidadAire/Main/Main.js
@@ -1,6 +1,6 @@
 import "./Main.css";
-import { ubicaciones } from "../../assets/ubicaciones";
-import { ubicacionesCV } from "../../assets/ubicacionesCV";
+import { ubicaciones } from "../../../assets/ubicaciones";
+import { ubicacionesCV } from "../../../assets/ubicacionesCV";
 import { useState } from "react";
 import { SiWindicss } from "react-icons/si";
 import { MdCo2 } from "react-icons/md";
@@ -10,15 +10,15 @@ import "leaflet/dist/leaflet.css";
 import { Marker } from "react-leaflet";
 import L from "leaflet";
 import { RiCloseCircleFill } from "react-icons/ri";
-import Detalles from "../../components/Detalles";
-import AyudaCAE from "../../components/AyudaCAE";
-import AyudaCAI from "../../components/AyudaCAI";
-import DetallesI from "../../components/DetallesI";
+import Detalles from "../../../components/Detalles";
+import AyudaCAE from "../../../components/AyudaCAE";
+import AyudaCAI from "../../../components/AyudaCAI";
+import DetallesI from "../../../components/DetallesI";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import { rangoscae } from "../../assets/rangoscae";
-import { rangoscai } from "../../assets/rangoscai";
-import DetallesIC from "../../components/DetallesIC";
-import Sidebar from "./Sidebar/Sidebar";
+import { rangoscae } from "../../../assets/rangoscae";
+import { rangoscai } from "../../../assets/rangoscai";
+import DetallesIC from "../../../components/DetallesIC";
+import Sidebar from "../Sidebar/Sidebar";
 
 const ubiCentro = ["-12.020381", "-77.049178"];
 
