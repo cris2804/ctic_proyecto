d3c2263d25498ccd43a81ddb2978ab1bedec0beb
Royer Lopez Flores <72278068+RoyerLopezFlores@users.noreply.github.com>
2023-09-10 20:11:03 -0500

    cambiando host

--- src/server/Getip.js
+++ src/server/Getip.js
@@ -1,8 +1,8 @@
 
 
 export const Getip = (host="") => {
-  //let hostactual = "https://www.beegons.com";
-  let hostactual = "http://localhost:3003";
+  let hostactual = "https://www.beegons.com";
+  //let hostactual = "http://localhost:3003";
   return hostactual
 }
 export const GetipTR = (host = "") =>{
