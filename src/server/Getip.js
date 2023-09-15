9ed7d4614112ecbc0d3ad28627e061fd56a332c2
Royer Lopez Flores <72278068+RoyerLopezFlores@users.noreply.github.com>
2023-09-14 23:26:02 -0500

    add interface alerta

--- src/server/Getip.js
+++ src/server/Getip.js
@@ -2,11 +2,11 @@
 
 export const Getip = (host="") => {
   let hostactual = "https://www.beegons.com";
-  //let hostactual = "http://localhost:3003";
+  //let hostactual = "http://localhost:8060";
   return hostactual
 }
 export const GetipTR = (host = "") =>{
-  //const hostactual = "http://localhost:3003"
+  //const hostactual = "http://localhost:8060"
   const hostactual = "http://3.229.175.160:8060/";
   return hostactual;
 }
